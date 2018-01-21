e7f1b33854791c258f7170a00187decfe252060c
virtimus <virtimus@gmail.com>
2018-01-21 15:09:42 +0100

    Update i3c-run.sh

--- dockerfiles/i3c/i3c-run.sh
+++ dockerfiles/i3c/i3c-run.sh
@@ -8,7 +8,7 @@ docker run -d --name $cName \
 		-v $i3cLogDir/$cName:/log \
 		-v /var/run/docker.sock:/var/run/docker.sock \
 		-e VIRTUAL_HOST=$cName.$i3cInHost,$cName.$i3cExHost \
-		-e VIRTUAL_PORT=80 \		
+		-e VIRTUAL_PORT=80 \
 		-e I3C_HOST=$i3cHost \
 		-e I3C_HOME=$i3cHome \
 		-e I3C_DATA_DIR=/data \
