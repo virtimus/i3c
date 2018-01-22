3b747d93da5e9cd1bd1e397347d58150cf2b1948
virtimus <virtimus@gmail.com>
2018-01-22 19:59:49 +0100

    Update i3c-run.sh

--- dockerfiles/portainer/i3c-run.sh
+++ dockerfiles/portainer/i3c-run.sh
@@ -5,6 +5,7 @@ cName=portainer
 
 docker run -d --name $cName \
 		-p 9000:9000 \
+		-e I3C_LOCAL_ENDPOINT=$I3C_LOCAL_ENDPOINT \
 		-e VIRTUAL_HOST=$cName.$i3cInHost,$cName.$i3cExHost \
 		-e VIRTUAL_PORT=9000 \
 		-v /var/run/docker.sock:/var/run/docker.sock \
