5f9c5d8d5a3c74a37be14856cbbd364496930bfe
virtimus <virtimus@gmail.com>
2018-01-27 14:15:10 +0100

    Update waitReady.sh

--- dockerfiles/i3c/waitReady.sh
+++ dockerfiles/i3c/waitReady.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
-while [ ! -e /i && seconds < $1 ] 
+seconds=30000
+if [ "x$1" != "x" ]; then
+   seconds=$1
+fi
+while [ ! -e /i && seconds > 0 ] 
 do
     echo "i3c/waitReady.sh - waiting for /i to appear ..."
     sleep 1000
-	seconds += 1000
-    #/bin/bash
+    seconds -= 1000
 done
