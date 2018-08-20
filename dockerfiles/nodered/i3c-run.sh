672a97be623a97c2c1345c2ea6810dbfeab6039a
root <root@mblapd.localdomain>
2018-08-20 19:46:09 +0200

    node-red

---
+++ dockerfiles/nodered/i3c-run.sh
@@ -0,0 +1,14 @@
+
+#!/bin/bash
+
+
+
+dParams="-p 1880:1880" 
+
+
+#doCommand=false
+
+i3cAfter(){
+	#docker exec i3cp sh -c "cd /servicebot-deploy/ && docker-compose up -d"
+	echo "access nodeRed at http:[host]:1880"
+}
---
+++ dockerfiles/nodered/readme.md
@@ -0,0 +1,7 @@
+
+
+refs:
+
+https://nodered.org/docs/platforms/docker
+http://noderedguide.com/
+https://www.coursera.org/learn/developer-nodered
