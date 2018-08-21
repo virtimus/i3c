beb9f7b0b2535a85c63783e7fcdbaaf6879b0f91
mb <mb@mblapd>
2018-08-21 21:04:56 +0200

    chmod

--- dockerfiles/nodered/i3c-run.sh
+++ dockerfiles/nodered/i3c-run.sh
@@ -11,6 +11,7 @@ i3cAfter(){
 	#docker exec i3cp sh -c "cd /servicebot-deploy/ && docker-compose up -d"
 	#echo "=======================runing link"
 	docker exec -u 0 nodered sh -c 'ln -s /i3c/i3c.sh /i'
+	docker exec -u 0 nodered sh -c 'chmod a+rw /var/run/docker.sock'
 	if [ ${i3cConfig[keepRunning]} -eq 1 ]; then
 		echo '#generated from i3c/nodered\n /i crun nodered' > /i3c/data/i3c/periodic/15min/checkNodeRedIsRunning
 	fi	
