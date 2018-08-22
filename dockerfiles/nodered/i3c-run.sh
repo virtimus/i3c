f11b581489910d2ebc46c13e799bbfb316463262
mb <mb@mblapd>
2018-08-22 16:31:01 +0200

    f

--- dockerfiles/nodered/i3c-run.sh
+++ dockerfiles/nodered/i3c-run.sh
@@ -2,7 +2,7 @@
 
 #-d -p 1880:1880 \
 dParams="-e VIRTUAL_PORT=1880 \
-	-v $i3cDataDir/$iName/nr.data:/data" 
+	-v $i3cDataDir/$cName/nr.data:/data" 
 addIParams=true
 
 #doCommand=false
@@ -10,10 +10,10 @@ addIParams=true
 i3cAfter(){
 	#docker exec i3cp sh -c "cd /servicebot-deploy/ && docker-compose up -d"
 	#echo "=======================runing link"
-	docker exec -u 0 $iName sh -c 'ln -s /i3c/i3c/i3c.sh /i'
-	docker exec -u 0 $iName sh -c 'chmod a+rw /var/run/docker.sock'
+	docker exec -u 0 $cName sh -c 'ln -s /i3c/i3c/i3c.sh /i'
+	docker exec -u 0 $cName sh -c 'chmod a+rw /var/run/docker.sock'
 	if [ ${i3cConfig[keepRunning]} -eq 1 ]; then
-		echo '#generated from i3c/nodered\n /i crun '$iName > /i3c/data/i3c/periodic/15min/checkNodeRedIsRunning
+		echo '#generated from i3c/nodered\n /i crun '$cName > /i3c/data/i3c/periodic/15min/checkNodeRedIsRunning
 	fi	
 	echo "access nodeRed at http:[host]:1880"
 }
