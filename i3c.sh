d74bb18b619a30c1608886d527cc86a3cb426823
mb <mb@mblapd>
2018-08-24 18:47:11 +0200

    licence

--- i3c.sh
+++ i3c.sh
@@ -7,7 +7,7 @@
 #
 #  !This software is free to use & modify as long as You will preserve this copyright notice! 
 #
-#  Copyright© 2014-2016 Mark Bisz (virtimus@gmail.com)
+#  Copyright (c) 2014-2016 Mark Bisz (virtimus@gmail.com)
 #
 ########################################################################################################
 
