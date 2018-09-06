ad7eff92e52d84f5c81c0d04e5b3a6af254f4dc2
virtimus <virtimus@gmail.com>
2018-09-06 19:17:33 +0200

    Update bootstrap-wsl.sh

--- bootstrap-wsl.sh
+++ bootstrap-wsl.sh
@@ -3,24 +3,36 @@
 if [ ! -e /i3c ]; then
 	secs=10;
 	echo "###################################################################################";
+	echo "";
 	echo " i3c.cloud bootstrap-wsl.sh script - running installation of docker components ... "
-	echo ""
+	echo "";
 	echo " Press 'q' to quit installation, any other key to continue. $secs timeout.  "
+	echo "";
 	echo "###################################################################################";
-	read -n 1 =t $secs -p "Input Selection:" mainmenuinput
+	read -n 1 -t $secs -p "Input Selection:" mainmenuinput
 	case "$mainmenuinput" in
 		q)
 		exit 0;
 		;;
 		*)
+		echo "";
 		echo "Setup processing ..."
 	esac
 
 	sudo apt-get update
 	sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common
-	
-	echo "ln -s /mnt/c/i3cRoot /i3c"
-	#ln -s /mnt/c/i3cRoot /i3c
-	
+	sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+	sudo apt-key fingerprint 0EBFCD88
+	sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+	sudo apt-get update
+	sudo apt-get -y install docker-ce
+	echo "###################################################################################";
+	echo "";
+	echo "Making root i3c folder (/i3c) ..."
+	ln -s /mnt/c/i3cRoot /i3c
+	echo "###################################################################################";
+	echo "";
+	echo "Runing main i3c/bootstrap.sh script ..."	
+	curl -sSL https://raw.githubusercontent.com/virtimus/i3c/master/bootstrap.sh | bash
 	
 fi
