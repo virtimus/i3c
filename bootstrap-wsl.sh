03b3c37d4af18c6cd51f3a4fd84b9010d0ad8035
virtimus <virtimus@gmail.com>
2018-09-08 18:35:36 +0200

    Update bootstrap-wsl.sh

--- bootstrap-wsl.sh
+++ bootstrap-wsl.sh
@@ -6,12 +6,12 @@ echo "   BOOTSTRAP-WSL.SH - Start ..."
 echo "###################################################################################";
 
 dtnow=$(date +%Y%m%d%H%M%S);
-if [ -e /i ]; then	
+if [ -L /i ]; then	
 	fto1="/i.$dtnow.bak";
 	echo "/i exists - moving to $fto1";
  	mv /i "$fto1"
 fi
-if [ -e /i3c ]; then
+if [ -L /i3c ]; then
 	fto1="/i3c.$dtnow.bak";
 	echo "/i3c exists - moving to $fto1";
  	mv /i3c "$fto1"
