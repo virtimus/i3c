686a653668e862afc21bf9d0970d7b9f5a3901f3
mb <mb@mblap-5559>
2018-10-22 22:54:19 +0200

    textConv folder build

--- dockerfiles/mint/run-startup.sh
+++ dockerfiles/mint/run-startup.sh
@@ -4,6 +4,15 @@ export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/ga
 
 service dbus start
 
+#install some tools for txt conversion service
+if [ ! -e /i3c/.shared/mint/toTxt ]; then
+	mkdir /i3c/.shared/mint/toTxt
+fi
+if [ ! -e /i3c/.shared/mint/toTxtOut ]; then
+	mkdir /i3c/.shared/mint/toTxtOut
+fi
+chown -R a+w /i3c/.shared/mint
+
 apt-get install -y incron xpdf
 echo root > /etc/incron.allow
 incrontab /incrontab
