22332311a31fad706a0bc9f8badf753d22363566
Virtimus <virtimus@gmail.com>
2019-04-11 10:46:33 +0200

    problem with download handler

--- dockerfiles/mint/i3c-build.sh
+++ dockerfiles/mint/i3c-build.sh
@@ -9,9 +9,16 @@ if [[ "$(docker images -q mint19 2> /dev/null)" == "" ]]; then
 	if [ ! -e /i3c/.dockerimages/mint19.i3czi ]; then
 		cd /i3c/.dockerimages
 		
-		wget --no-check-certificate  https://ncl.i3c.pl/s/W9sFy22J8jzFZp4/download
+		downloadUrl="https://ncl.i3c.pl"
 		
-		mv download mint19.i3czi
+		wget --no-check-certificate  $downloadUrl/s/W9sFy22J8jzFZp4/download
+		ret=$?;
+		if [ $ret -eq 0 ];then
+			mv download mint19.i3czi
+		else
+			echo "Problem downloading base mint19 image from $downloadUrl - try once again"
+			exit ret;
+		fi	
 	fi
 	
 	/i load mint19	
