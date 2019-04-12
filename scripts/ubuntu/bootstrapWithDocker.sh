65ffd9a6173b80f5e714eba0f612d0ff55291194
Virtimus <virtimus@gmail.com>
2019-04-12 14:30:19 +0200

    bootstrap with docker install

---
+++ scripts/ubuntu/bootstrapWithDocker.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+curl -sSL https://raw.githubusercontent.com/virtimus/i3c/master/scripts/ubuntu/installDocker.sh
+ret=$?;
+if [ $ret -ne 0 ]; then  
+	echo "Docker instalaction failed ?"
+fi
+curl -sSL https://raw.githubusercontent.com/virtimus/i3c/master/bootstrap.sh | bash
