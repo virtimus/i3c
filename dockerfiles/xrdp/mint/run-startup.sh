264deee2dd2949abeb416bcd1dbc09e907d53566
Virtimus <virtimus@gmail.com>
2019-04-12 17:21:52 +0200

    group dockerh

--- dockerfiles/xrdp/mint/run-startup.sh
+++ dockerfiles/xrdp/mint/run-startup.sh
@@ -4,6 +4,9 @@ service dbus start
 
 chmod -R a+w /i3c/.shared/mint
 
+sudo groupadd -g 999 dockerh
+sudo usermod -a -G dockerh mb
+
 ln -sf /i3c/i3c/i3c.sh /i
 
 if [ -e /i3c/.overrides/run-startup.sh ]; then
