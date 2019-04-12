c4f06369ea3a82472557df66ad7c3c53800a9672
Virtimus <virtimus@gmail.com>
2019-04-12 19:17:19 +0200

    data/log dir

--- dockerfiles/xrdp/mint/i3c-run.sh
+++ dockerfiles/xrdp/mint/i3c-run.sh
@@ -2,5 +2,7 @@
 addIParams=true;
 dParams="-d --privileged --hostname terminalserver --shm-size 1g -p 3389:3389 -p 2222:22 \
 	$addParams \
+	-v $i3cDataDir:/i3c/i3c.data \
+	-v $i3cLogDir:/i3c/i3c.log \
 	-v $uData/home:/home \
 	-v $uData/etc/ssh:/etc/ssh"
