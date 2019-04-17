958b91d33b8f77eee81222542e5903cad1af3236
Virtimus <virtimus@gmail.com>
2019-04-17 11:56:05 +0200

    add npm

--- dockerfiles/xrdp/mint/i3c-run.sh
+++ dockerfiles/xrdp/mint/i3c-run.sh
@@ -4,3 +4,9 @@ dParams="-d --privileged --hostname terminalserver --shm-size 1g -p 3389:3389 -p
 	$addParams \
 	-v $uData/home:/home \
 	-v $uData/etc/ssh:/etc/ssh"
+	
+	
+i3cAfter(){
+	
+	/i exshd xrdp/mint "su - mb && npm install -g gulp hjson";
+}	
