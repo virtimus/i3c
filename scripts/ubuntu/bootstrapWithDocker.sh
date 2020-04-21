6e632fe2d5c7b336a25ecfd8b196caa049527d98
virtimus <virtimus@gmail.com>
2020-04-21 19:46:54 +0200

    bootstrap fix

--- scripts/ubuntu/bootstrapWithDocker.sh
+++ scripts/ubuntu/bootstrapWithDocker.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+sudo apt-get update && \
+		apt-get -y install net-utils lsb-release
+
 curl -sSL https://raw.githubusercontent.com/virtimus/i3c/master/scripts/ubuntu/installDocker.sh | bash
 ret=$?;
 if [ $ret -ne 0 ]; then  
--- scripts/ubuntu/installDocker.sh
+++ scripts/ubuntu/installDocker.sh
@@ -10,19 +10,19 @@ if [ $SYSREL == 'tara' ]; then
 	SYSREL='bionic';
 fi
 	
-apt-get update && \
-		apt-get -y install apt-transport-https \
+sudo apt-get update && \
+		sudo apt-get -y install apt-transport-https \
      	ca-certificates \
      	curl \
      	gnupg2 \
      	software-properties-common && \
 		curl -fsSL https://download.docker.com/linux/$SYSID/gpg > /tmp/dkey; apt-key add /tmp/dkey && \
-		add-apt-repository \
+		sudo add-apt-repository \
    		"deb [arch=amd64] https://download.docker.com/linux/$SYSID \
   	 	$SYSREL \
    		stable" && \
-		apt-get update && \
-		apt-get -y install docker-ce
+		sudo apt-get update && \
+		sudo apt-get -y install docker-ce
 		
 		
 export DOCKER_COMPOSE_VERSION=1.22.0	
